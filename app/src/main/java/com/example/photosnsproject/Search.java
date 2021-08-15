b37c1100aa7dbe419bd30175f095e3be7e7febd0
seungsooo <dave5104@naver.com>
2021-08-15 17:25:48 +0900

    123

--- app/src/main/java/com/example/photosnsproject/Search.java
+++ app/src/main/java/com/example/photosnsproject/Search.java
@@ -25,10 +25,14 @@ public class Search extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private ArrayList<User> list;
+    private ArrayList<String> list2;
     private ArrayList<User> wholelist;
+    private ArrayList<PostItem> wholelist2;
     private FirebaseDatabase database;
     private DatabaseReference db;
+    private String temp;
     EditText search_edit;
+    EditText search_tag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +40,24 @@ public class Search extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         search_edit=findViewById(R.id.search_edit);
+        search_tag=findViewById(R.id.search_tag);
         recyclerView=findViewById(R.id.search_rcy);
         recyclerView.setHasFixedSize(true); //정리한번
         layoutManager= new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         list=new ArrayList<>();
+        list2=new ArrayList<>();
         wholelist=new ArrayList<>();
+        wholelist2=new ArrayList<>();
         adapter=new SearchAdapter(list, this);
         recyclerView.setAdapter(adapter);
         database=FirebaseDatabase.getInstance();
         db=database.getReference();
 
+
         list.clear();
+        list2.clear();
+
 
         db.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -56,6 +66,48 @@ public class Search extends AppCompatActivity {
                 for (DataSnapshot sn : snapshot.getChildren()){
                     User user = sn.getValue(User.class); //1
                     wholelist.add(user);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+         db.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                wholelist2.clear();
+                for (DataSnapshot sn : snapshot.getChildren()){
+                    User user=sn.getValue(User.class);
+                    String strId = user.getId();
+                    db.child("Users").child(strId).child("post").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for(DataSnapshot sn2 : snapshot.getChildren()) {
+                                PostItem postitem = sn2.getValue(PostItem.class);
+
+                                try {
+                                    ArrayList<String> arrTag = postitem.getTag();
+
+                                    for(int i=0; i<arrTag.size(); i++)
+                                    {
+                                        wholelist2.add(postitem);
+                                    }
+                                }
+
+                                catch(NullPointerException e) {
+
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
             }
 
@@ -85,7 +137,27 @@ public class Search extends AppCompatActivity {
             }
         });
 
+        // 태그를 이용한 검색
 
+        search_tag.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String string_tag;
+                string_tag=search_tag.getText().toString();
+                search_text2(string_tag);
+
+            }
+        });
 
 
     }
@@ -98,6 +170,25 @@ public class Search extends AppCompatActivity {
                 list.add(wholelist.get(i));
             }
         }
+
         adapter.notifyDataSetChanged(); //데이터 바뀐걸 어뎁터한테 알려줌.
     }
+
+
+    public void search_text2(String s){
+        list2.clear();
+        for(int i=0; i<wholelist2.size(); i++){
+
+            for(int j=0; j<wholelist2.get(i).getTag().size(); j++)
+            {
+                String strTag=wholelist2.get(i).getTag().get(j);
+                if(strTag.contains(s)) {
+                    list2.add(strTag);
+                }
+            }
+        }
+
+        adapter.notifyDataSetChanged(); //데이터 바뀐걸 어뎁터한테 알려줌.
+    }
+
 }
