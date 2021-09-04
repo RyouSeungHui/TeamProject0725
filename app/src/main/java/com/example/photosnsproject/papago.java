5d382dd562b53dda6d734114a0c31b854a517ebf
hoseongseong <hsv1961@naver.com>
2021-09-04 18:30:39 +0900

    포스팅 1차완료

---
+++ app/src/main/java/com/example/photosnsproject/papago.java
@@ -0,0 +1,58 @@
+package com.example.photosnsproject;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class papago {
+
+    public static String translation(String word,String source,String target) {
+        String clientId = "BnitH_CVzDRTbPWVEZNd";
+        String clientPw = "Ld2lUhiRsd";
+        try {
+            String wordSource, wordTarget;
+            String text = URLEncoder.encode(word, "UTF-8");             //word
+            wordSource = URLEncoder.encode(source, "UTF-8");
+            wordTarget = URLEncoder.encode(target, "UTF-8");
+
+            String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientPw);
+
+            String postParams = "source="+wordSource+"&target="+wordTarget+"&text=" + text;
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            String s = response.toString();
+            s = s.split("\"")[27];
+            return s;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "0";
+    }
+}
